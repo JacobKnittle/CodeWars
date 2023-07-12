39b39933d84eb1dcd385e4f29dfdcce5321b4c9e
Jacob Knittle <jdknittle@live.com>
2023-07-12 13:38:37 -0700

    add twoOldestAges solution

---
+++ solutions/twoOldestAges.js
@@ -0,0 +1,13 @@
+//p: array of numbers of at least two, can have repeats, any order
+//r: second oldest age, oldest age in an array
+//e: [1,5,87,45,8,8] should return [45, 87]
+
+// const twoOldestAges = (ages) => {
+//   const sortedAges = ages.sort((a, b) => a - b);
+//   return [sortedAges[sortedAges.length - 2], sortedAges[sortedAges.length - 1]];
+// };
+
+const twoOldestAges = (ages) => ages.sort((a, b) => a - b).slice(-2);
+
+const result = twoOldestAges([1, 5, 87, 45, 8, 8]);
+console.log(result);
