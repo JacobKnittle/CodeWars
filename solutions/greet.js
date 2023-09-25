7d9480595e7c4b34643ca2da27b6bcafe5eafb8d
Jacob Knittle <jdknittle@live.com>
2023-09-24 21:03:15 -0700

    add greet solution

---
+++ solutions/greet.js
@@ -0,0 +1,11 @@
+//p: string of name. can be upper or lowercase char, number, special char
+//r: string of Hello then the name with the first letter capitalized the rest lowercase and a ! at the end
+//e: 'jAcob' => 'Hello Jacob!'
+//p: remove the first char capitalize it and lowercase the rest. Join them back together. return a template literal with the name variable
+
+const greet = (name) => {
+  return `Hello ${name[0].toUpperCase() + name.slice(1).toLowerCase()}!`;
+};
+
+const result = greet('jAcob');
+console.log(result);
