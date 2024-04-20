7209f313bb25c3885abb4056a67adc5e25025609
Jacob Knittle <jdknittle@live.com>
2024-04-20 11:38:28 -0700

    classyClasses

---
+++ solutions/classyClasses.js
@@ -0,0 +1,11 @@
+class Person {
+	constructor(name, age) {
+		this._name = name;
+		this._age = age;
+	}
+	get info() {
+		return `${this._name}s age is ${this._age}`;
+	}
+}
+
+var john = new Person('john', 34);
