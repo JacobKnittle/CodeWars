9a680776a8cb187996b1d64922b19b6cff8cd385
Jacob Knittle <jdknittle@live.com>
2024-05-31 15:47:58 -0700

    animalObject

--- solutions/animalObject.js
+++ solutions/animalObject.js
@@ -1,6 +1,6 @@
 // animal object with 3 properties name, legs, and color
 // return string like "This white dog has 4 legs"
 
-const animal = (obj) => `This ${obj.color} ${obj.name} has ${obj.legs} legs`;
+const animal = (obj) => `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
 
 console.log(animal({ name: 'cat', legs: 'three', color: 'orange' }));
