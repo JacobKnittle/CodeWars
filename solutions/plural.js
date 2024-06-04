f030e3bc04c27912842dafbf95bac2e063ff3088
Jacob Knittle <jdknittle@live.com>
2024-06-04 16:31:02 -0700

    plural

---
+++ solutions/plural.js
@@ -0,0 +1,8 @@
+// input is 0 or positive float or int
+// output true if it is 2 or more
+
+// return if else with the condition n >= 2
+const plural = (n) => n != 1;
+
+console.log(plural(1.8));
+console.log(plural(3));
