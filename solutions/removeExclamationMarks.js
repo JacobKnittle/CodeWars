3ee741e1276f4b71b5d4a0a07590783cd606ff81
Jacob Knittle <jdknittle@live.com>
2024-06-12 11:30:03 -0700

    removeExclamationMarks

---
+++ solutions/removeExclamationMarks.js
@@ -0,0 +1,6 @@
+// given a str with exclamation marks
+// remove all of them
+
+const removeExclamationMarks = (str) => str.replaceAll('!', '');
+
+console.log(removeExclamationMarks('!hello!'), 'hello');
