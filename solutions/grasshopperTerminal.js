2910147cb8025e99d512ce8574f41450087ff005
Jacob Knittle <jdknittle@live.com>
2024-06-13 10:57:29 -0700

    grasshopperTerminal

---
+++ solutions/grasshopperTerminal.js
@@ -0,0 +1,8 @@
+// two params position number and roll number of char between 1-6
+// return the position times the roll * 2
+
+function move(position, roll) {
+	return position + roll * 2;
+}
+
+console.log(move(0, 5), 10);
