f583a76f884415d6a7c2e68b741300c852135348
Jacob Knittle <jdknittle@live.com>
2024-06-13 10:52:06 -0700

    testEven

---
+++ solutions/testEven.js
@@ -0,0 +1,10 @@
+// num into function
+// return true if it is an even number
+
+function testEven(n) {
+	return n % 2 === 0;
+}
+
+console.log(testEven(0), true);
+console.log(testEven(1), false);
+console.log(testEven(2), true);
